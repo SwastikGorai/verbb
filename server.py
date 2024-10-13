0d70bacddadc11bbb24cf513ff92a74c915cb233
Swastik <swastikgorai@gmail.com>
2024-10-14 00:34:18 +0530

    chore: requirements txt

--- server.py
+++ server.py
@@ -3,5 +3,5 @@ import uvicorn
 from src.main import app
 
 if __name__ == "__main__":
-    port = os.getenv("PORT") or 8080
-    uvicorn.run(app, host="127.0.0.1", port=int(port))
+    port = os.getenv("PORT") or 8181
+    uvicorn.run(app, host="0.0.0.0", port=int(port))
